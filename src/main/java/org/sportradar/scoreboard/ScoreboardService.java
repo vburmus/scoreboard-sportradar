33faf0fb5ecb850c046f4e9e858c9b7e031cf0e9
Volodymyr Burmus <burmusvolod@gmail.com>
2024-07-23 17:01:55 +0200

    feat(scoreboard) initial project setup

---
+++ src/main/java/org/sportradar/scoreboard/ScoreboardService.java
@@ -0,0 +1,22 @@
+package org.sportradar.scoreboard;
+
+import java.util.List;
+
+public class ScoreboardService {
+
+    public void startMatch(String homeTeam, String awayTeam) {
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+
+    public void updateScore(String homeTeam, String awayTeam, int homeScore, int awayScore) {
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+
+    public void finishMatch(String homeTeam, String awayTeam) {
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+
+    public List<String> getSummary() {
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+}
---
+++ src/test/java/org/sportradar/scoreboard/ScoreboardServiceTest.java
@@ -0,0 +1,12 @@
+package org.sportradar.scoreboard;
+
+import org.junit.jupiter.api.BeforeEach;
+
+class ScoreboardServiceTest {
+    private ScoreboardService service;
+
+    @BeforeEach
+    void setUp() {
+        service = new ScoreboardService();
+    }
+}
